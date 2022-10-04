561524b7777e8832d2b5fe59b23ca463f20c905e
danny1707 <dany_170799@outlook.com>
2022-10-04 11:25:14 -0500

    configuracion de serverjs

---
+++ server.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import bcrypt from 'bcrypt'
+import stripe from 'stripe'
+import { initializeApp } from 'firebase/app'
+
+//Configuración de Firebase
+const fiirebaseConfig = {
+
+}
