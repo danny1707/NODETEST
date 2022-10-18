bd3121270d5f73e1af575c7386db122cc7a09ccd
danny1707 <dany_170799@outlook.com>
2022-10-18 10:17:17 -0500

    se agrregaron nuevos apartados de funcionalidad y aspecto

---
+++ public/js/nav.js
@@ -0,0 +1,101 @@
+const navbar = document.querySelector('.navbar')
+
+
+window.addEventListener('scroll', () => {
+  console.log('scroll', scrollY);
+  if(scrollY => 180){ 
+    navbar.classList.add('bg')
+  }else{
+    navbar.classList.remove('bg')
+  }
+})
+
+const createNavbar = () => {
+  navbar.innerHTML += 
+  `
+    <ul class="links-container">
+
+      <li class="link-item">
+        <a href="#" class="link active">Home</a>
+      </li>
+
+      <li class="link-item">
+        <a href="#" class="link active">Product</a>
+      </li>
+
+      <li class="link-item">
+        <a href="#" class="link">About</a>
+      </li>
+
+      <li class="link-item">
+        <a href="#" class="link">Contact</a>
+      </li>
+
+    </ul>
+
+    <div class="user-interactions">
+
+      <div class="search-box"> 
+        <input type="text" class="search" placeholder="search item">
+
+        <button class="search-btn">
+          <img src="./img/search.png" alt="" class="cart-icon">
+        </button>
+
+      </div>
+    
+      <div class="cart" onclick="location.href = '/cart' ">
+
+        <img src="./img/cart.png" class="cart-icon">
+        <span class="cart-item-count">00</span>
+
+      </div>
+
+      <div class="user">
+
+        <img src="./img/user.png" class="user-icon">
+
+        <div class="user-icon-popup">
+          <p>Login to your acount</p>
+          <p>Login</p>
+        </div>
+
+      </div>
+
+    </div>
+  `
+}
+
+createNavbar()
+
+//user icon popup
+//Obtenemos los elementos
+let userIcon = document.querySelector('.user-icon')
+let userPopupIcon = document.querySelector('.user-icon-popup')
+
+userIcon.addEventListener('click', () => {
+  userPopupIcon.classList.toggle('active')
+})
+
+let text = userPopupIcon.querySelector('p')
+let actionBtn = userPopupIcon.querySelector('a')
+let user = JSON.parse(sessionStorage.user || null )
+
+if(user != null){
+  text.innerHTML = `log in as, ${user.name}`
+  actionBtn.innerHTML= 'Logout'
+  actionBtn.addEventListener('click', () => {
+    logout()
+  })
+} else {
+  text.innerHTML = `log in to your account`
+  actionBtn.innerHTML= 'Login'
+  actionBtn.addEventListener('click', () => {
+    location.href('/login')
+  })
+}
+
+const logout = () => {
+  sessionStorage.clear()
+  location.reload()
+}
--- server.js
+++ server.js
@@ -1,9 +1,42 @@
-import express from 'express'
-import bcrypt from 'bcrypt'
-import stripe from 'stripe'
-import { initializeApp } from 'firebase/app'
+import express from 'express';
+import bcrypt from 'bcrypt';
+import stripe from 'stripe';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore'
 
-//Configuración de Firebase
-const fiirebaseConfig = {
+
+//Configuracion de Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDg3kBvy9nG85knCMuVhj6b6FV1w7eZ1Fk",
+
+  authDomain: "ecommerce-31e2a.firebaseapp.com",
+
+  projectId: "ecommerce-31e2a",
+
+  storageBucket: "ecommerce-31e2a.appspot.com",
+
+  messagingSenderId: "301203238732",
+
+  appId: "1:301203238732:web:1c8ef29bcda382665469e9"
 
 }
+
+const firebase = initializeApp(firebaseConfig)
+const db = getFirestore()
+//Inicializacion del servidor
+const app = express()
+
+//middleware
+app.use(express.static('public'))
+app.use(express.json())//Permite compartir forms
+
+//Rutas
+
+//Home
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: 'public' })
+})
+
+app.listen(3000, () => {
+  console.log('Servidor en Ejecucion...');
+})
